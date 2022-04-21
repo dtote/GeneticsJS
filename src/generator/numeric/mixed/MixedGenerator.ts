c9256c6e237d40f8efd7ac4e0b471c60a6f39922
Nestor Torres <alu0101021768@ull.edu.es>
2022-04-21 13:48:29 +0100

    feat: modified mixed generator to generate numbers in the expected range

--- src/generator/numeric/mixed/MixedGenerator.ts
+++ src/generator/numeric/mixed/MixedGenerator.ts
@@ -3,17 +3,19 @@
  * Copyright (c) 2019 Cristian Abrante. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
-
+import { NumericRange } from '../../../individual';
 import { MixedIndividual } from '../../../individual/numeric/mixed';
 import { Generator } from '../../utils';
 import { NumericGenerator } from '../base';
 import { NumericParams } from '../base/NumericGenerator';
+import { RANGES } from './../../../optimization/constants/Ranges';
 
 /**
  * ## Mixed generator
  * Generates a [[MixedIndividual]].
  */
 export class MixedGenerator extends NumericGenerator<MixedIndividual> {
+  static currentGeneIndex = 0;
   /**
    * Generates a gene with the specified
    * params.
@@ -21,7 +23,16 @@ export class MixedGenerator extends NumericGenerator<MixedIndividual> {
    * @return the generated gene.
    */
   public generateGene(params: NumericParams): number {
-    return Generator.generateMixed(params.range, params.engine);
+    const geneRange = RANGES[MixedGenerator.currentGeneIndex % 28];
+    console.log({ geneRange });
+    const generatedIndividual = Generator.generateMixed(
+      new NumericRange(geneRange.lower, geneRange.upper),
+      params.engine,
+    );
+    MixedGenerator.currentGeneIndex++;
+    console.log({ generatedIndividual: generatedIndividual.toFixed() });
+
+    return generatedIndividual;
   }
 
   /**
