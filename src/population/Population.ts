6afa9ae24bff45d33863c8fd6d5e3b2355e1fc2d
Nestor Torres <alu0101021768@ull.edu.es>
2022-04-21 13:45:59 +0100

    feat: modified fitness selection function, to select lower values as the fittest ones instead of higher values

--- src/population/Population.ts
+++ src/population/Population.ts
@@ -34,7 +34,7 @@ export class Population<I extends BaseIndividual<T>, T> {
     currentBestFitness: number,
     newElementFitness: number,
   ) => {
-    return newElementFitness > currentBestFitness;
+    return newElementFitness < currentBestFitness;
   };
 
   public populationStatistics: PopulationStatistics = {
